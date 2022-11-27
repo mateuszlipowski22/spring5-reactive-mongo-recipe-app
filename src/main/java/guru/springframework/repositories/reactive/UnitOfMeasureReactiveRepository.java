c9447667cdf6e2428effeb24ec4c39ecb0fcd0ec
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-11-27 22:48:38 +0100

    Creation of RecipeReactiveRepository and CategoryReactiveRepository

--- src/main/java/guru/springframework/repositories/reactive/UnitOfMeasureReactiveRepository.java
+++ src/main/java/guru/springframework/repositories/reactive/UnitOfMeasureReactiveRepository.java
@@ -1,7 +1,13 @@
 package guru.springframework.repositories.reactive;
 
+import guru.springframework.domain.Category;
 import guru.springframework.domain.UnitOfMeasure;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
 
 public interface UnitOfMeasureReactiveRepository extends ReactiveMongoRepository<UnitOfMeasure,String> {
+
+    Mono<UnitOfMeasure> findByDescription(String description);
+
+
 }
